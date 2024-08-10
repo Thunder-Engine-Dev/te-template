31dbe39697cd9d0e7275831da3f90e413090757b
reflexguru <trianglestudioad@gmail.com>
2024-08-10 06:00:25 +0300

    new project management script & remove pullers

--- pullers.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git reset --hard
-cd engine
-git reset --hard
-cd ..
-git pull
-git submodule update --remote
--- pullers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git reset --hard
-cd engine
-git reset --hard
-cd ..
-git pull
-git submodule update --remote
